323a8327235b01ada5d213eed60658fbc1d3c886
telewood <telewood@users.noreply.github.com>
2016-05-18 02:39:58 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {213344430},--Sudo users
+    sudo_users = { 114315301},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
